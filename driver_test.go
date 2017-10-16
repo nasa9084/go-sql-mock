f5bc691bc8cb00517c896baadfad487a94b9b93b
nasa9084 <nasa9084@live.jp>
2017-10-16 10:38:23 +0900

    add: expected rows tests

--- driver_test.go
+++ driver_test.go
@@ -2,7 +2,10 @@ package sqlmock_test
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"testing"
+
+	sqlmock "github.com/nasa9084/go-sql-mock"
 )
 
 func TestDriver(t *testing.T) {
@@ -12,4 +15,41 @@ func TestDriver(t *testing.T) {
 		return
 	}
 	defer db.Close()
+}
+
+func TestExpectedRows(t *testing.T) {
+	opts := []sqlmock.RowsOpts{
+		sqlmock.Columns([]string{"id", "name", "age"}),
+		sqlmock.ValuesList([][]driver.Value{
+			[]driver.Value{"something", "Alice", 10},
+		}),
+	}
+	db, err := sql.Open("sqlmock", "")
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	sqlmock.ExpectedRows(opts...)
+	row := db.QueryRow(`SELECT * FROM mock`)
+
+	var some struct {
+		ID   string
+		Name string
+		Age  int
+	}
+	if err := row.Scan(&some.ID, &some.Name, &some.Age); err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	if some.ID != "something" {
+		t.Errorf(`"%s" != "%s"`, some.ID, "something")
+		return
+	}
+	if some.Name != "Alice" {
+		t.Errorf(`"%s" != "%s"`, some.Name, "Alice")
+		return
+	}
+	if some.Age != 10 {
+		t.Errorf(`%d != %d`, some.Age, 10)
+		return
+	}
 }
